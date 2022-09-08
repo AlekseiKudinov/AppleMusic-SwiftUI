61c94ff90dbedb8bb1c09b1536dcbfb7181f475d
AlekseiKudinov <machette94@gmail.com>
2022-09-08 23:53:00 +0300

    Initial Commit

---
+++ AppleMusic-SwiftUI/AppleMusic_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_SwiftUIApp.swift
+//  AppleMusic-SwiftUI
+//
+//  Created by Алексей Кудинов on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
